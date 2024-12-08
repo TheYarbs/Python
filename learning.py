b052ce1a988b84ad91ee9353c8e4c41713c352d4
Andrew T. Yarbrough <andrew.t.yarbrough@gmail.com>
2024-12-08 12:26:16 -0500

    15 min into Youtube video

--- learning.py
+++ learning.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -12,4 +16,46 @@ def deposit():
             print("Please enter a number.")
 
     return amount
-deposit()
+
+def get_number_of_lines():
+    while True:
+        amount = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} -${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+
+def main():
+    balance = deposit()
+    lines - get_number_of_lines()
+    bet = get_bet()
+
+    print(balance, lines)
+
+
+
+
+main()
