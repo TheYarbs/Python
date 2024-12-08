19b24fac249e7eed31cacc9f223ff619ee3545af
Andrew T. Yarbrough <andrew.t.yarbrough@gmail.com>
2024-12-08 12:29:59 -0500

    15 min into youtube video

--- learning.py
+++ learning.py
@@ -52,10 +52,7 @@ def main():
     balance = deposit()
     lines - get_number_of_lines()
     bet = get_bet()
-
-    print(balance, lines)
-
-
+    print(f"You are betting ${bet} on [lines] lines. Total bet is equal to: $ {total_bet}")
 
 
 main()
