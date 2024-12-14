f4c075da3c09eed314226a825f37b46722506998
Andrew T. Yarbrough <andrew.t.yarbrough@gmail.com>
2024-12-13 23:55:31 -0500

    fixes

--- learning.py
+++ learning.py
@@ -46,13 +46,12 @@ def get_bet():
 
     return amount
 
-
-
 def main():
     balance = deposit()
     lines - get_number_of_lines()
     bet = get_bet()
     total_bet = bet * lines
     print(f"You are betting ${bet} on [lines] lines. Total bet is equal to: $ {total_bet}")
+
 
 main()
